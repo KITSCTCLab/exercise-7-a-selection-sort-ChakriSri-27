7eb7ccc3716462354e82cb7d16b223a3cfdac9f2
ChakriSri-27 <91770676+ChakriSri-27@users.noreply.github.com>
2022-09-12 15:25:52 +0530

    4

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ def selectionSort(array, size) -> List[int]:
       if array[i] < array[min_idx]:
         min_idx = i
     (array[step], array[min_idx]) = (array[min_idx], array[step])
-  
+  return array
 
 # Do not change the following code
 input_data = input()
